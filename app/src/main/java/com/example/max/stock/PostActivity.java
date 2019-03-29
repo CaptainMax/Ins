ce079e2ef6a847d53c93d267317a82760c3c4f4b
utafacss@gmail.com <utafacss2018>
2019-03-29 09:56:40 -0500

    First Update

---
+++ app/src/main/java/com/example/max/stock/PostActivity.java
@@ -0,0 +1,18 @@
+package com.example.max.stock;
+
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+
+
+    }
+}
